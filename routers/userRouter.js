a26ad0321b247a6727e5315f91d694361550c1d2
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-09-05 22:47:07 +0530

    implement bcrypt and try catch in user signin

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,29 +2,44 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const userRouter = express.Router();
 const { userModel } = require("../db");
+const { z } = require("zod");
 const signinHandler = function (req, res) {
     res.json({
         message: "user singin handler"
     });
 }
-
+const userInpuValidator = z.object({
+    email: z.email("provide valid email address"),
+    password: z.string("provide password as string"),
+    firstName: z.string("provide first name as string"),
+    lastName: z.string("provide last name as string"),
+})
 const signupHandler = async function (req, res) {
     console.log(req.body);
-    let { email, password, firstName, lastName } = req.body;
-    password = await bcrypt.hash(password, 10);
-    let type = "user";
-    const newUser = new userModel({
-        email, password, firstName, lastName, type
-    })
-    try {
-        await newUser.save();
-        res.json({
-            message: "user addedd successfully"
+    const isCorrectInputs = userInpuValidator.safeParse(req.body);
+    console.log(isCorrectInputs);
+    if (isCorrectInputs.success) {
+        let { email, password, firstName, lastName } = req.body;
+        password = await bcrypt.hash(password, 10);
+        let type = "user";
+        const newUser = new userModel({
+            email, password, firstName, lastName, type
         })
-    } catch (err) {
-        console.log("Error saving new user ", err);
+        try {
+            await newUser.save();
+            res.json({
+                message: "user addedd successfully"
+            })
+        } catch (err) {
+            console.log("Error saving new user ", err);
+            res.json({
+                message: "failed in adding user"
+            })
+        }
+    }
+    else {
         res.json({
-            message: "failed in adding user"
+            message: "Input validation failed"
         })
     }
 }
