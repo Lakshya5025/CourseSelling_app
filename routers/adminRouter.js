e02c10bd9da04a54dfd64353a35fd42b7120180b
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-09-09 22:36:30 +0530

    add course adding feature

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -2,23 +2,12 @@ const express = require('express');
 const jwt = require("jsonwebtoken");
 const adminRouter = express.Router();
 const { signinHandler, signupHandler } = require("../controllers/signController");
-const { userModel } = require('../db');
-
+const { userModel, courseModel } = require('../db');
 const setUserType = function (req, res, next) {
     req.userType = "admin"
     next();
 }
-const uploadCourseHandler = function (req, res) {
-    res.json({
-        message: "upload course handler"
-    });
-}
 
-const updateCourseHandler = function (req, res) {
-    res.json({
-        message: "upload course handler"
-    });
-}
 const auth = async function (req, res, next) {
     const token = req.cookies.token;
     if (!token) {
@@ -28,7 +17,7 @@ const auth = async function (req, res, next) {
     }
     try {
         const user = await jwt.verify(token, process.env.JWT_SECRET);
-        console.log(user);
+        // console.log(user);
         const { userId } = user;
         const findInDB = await userModel.findOne({
             _id: userId
@@ -47,10 +36,48 @@ const auth = async function (req, res, next) {
             message: err.message
         })
     }
-    next();
 }
+
+
+
+const uploadCourseHandler = async function (req, res) {
+    const { title, imageUrl, price, description } = req.body;
+    if (!title || !imageUrl || !price || !description) {
+        return res.json({
+            message: "provide (title, imageUrl, price, description)"
+        })
+    }
+    // console.log(req.user.userId);
+    let createrId = req.user.userId;
+    // console.log("hi");
+    const newCourse = new courseModel({
+        title, imageUrl, price, description, createrId
+    });
+    // console.log("after course model");
+    try {
+        await newCourse.save();
+        res.json({
+            message: "New course added"
+        })
+    }
+    catch (err) {
+        res.json({
+            message: "Database error"
+        })
+    }
+}
+
+const updateCourseHandler = function (req, res) {
+    res.json({
+        message: "upload course handler"
+    });
+}
+
+
+
+
 adminRouter.post("/signin", setUserType, signinHandler);
-adminRouter.post('/signup', auth, setUserType, signupHandler);
+adminRouter.post('/signup', setUserType, signupHandler);
 adminRouter.post("/courses", auth, uploadCourseHandler);
 adminRouter.put("/courses/", auth, updateCourseHandler);
 
